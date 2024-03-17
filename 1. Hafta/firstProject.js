e1229185e2400d53c72c103ea4a2949897b026e0
Tugay Tuna <tugaytuna@outlook.com.tr>
2024-03-17 21:10:50 +0300

    first week

---
+++ 1. Hafta/firstProject.js
@@ -0,0 +1,43 @@
+console.log("Merhaba Dünya!");
+console.log("1. Hafta");
+
+let isim = "Tugay";
+let isim2 = "Ercan";
+let age = 20;
+
+isim = "Ahmet";
+
+// bool - true / false
+let isAdmin = true;
+
+var lokasyon = "İstanbul";
+lokasyon = "Ankara";
+console.log(lokasyon);
+
+// read-online değişken oluşturmak için;
+const websitesi = "google.com";
+
+console.log(isim);
+console.log(isim2);
+console.log(age);
+console.log(isAdmin);
+
+console.log(isim, age);
+console.log(isim + " - " + age);
+
+console.log("İsim: " + isim2);
+
+console.log("---------------------");
+
+let sayi1 = 15;
+let sayi2 = 20;
+
+let topam;
+toplam = sayi1 + sayi2;
+
+let sonuc = sayi1 * sayi2;
+
+console.log(toplam);
+console.log(sonuc);
+
+console.log(sayi1 * sayi2);
