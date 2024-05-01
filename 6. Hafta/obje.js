f39219e007de081db5107f6a00193e421b6ba64d
Tugay Tuna <tugaytuna@outlook.com.tr>
2024-05-01 17:35:02 +0300

    prelessonp1

---
+++ 6. Hafta/obje.js
@@ -0,0 +1,19 @@
+let name = "Tugay";
+let surname = "Tuna";
+let age = 20;
+let isAdmin = true;
+let city = "İstanbul";
+let favorites = ["html", "css", "javascript"];
+
+// console.log(name, surname, age, isAdmin, city, favorites);
+
+let person1 = {
+  name: "Tugay",
+  surname: "Tuna",
+  age: 20,
+  isAdmin: true,
+  city: "İstanbul",
+  favorites: ["html", "css", "javascript"],
+};
+
+console.log(person1);
