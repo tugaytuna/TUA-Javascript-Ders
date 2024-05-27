e1592399def3c355820e97fea10375117c291c61
Tugay Tuna <tugaytuna@outlook.com.tr>
2024-05-27 22:18:13 +0300

    prelesson

---
+++ 8. Hafta/kod.js
@@ -0,0 +1,32 @@
+let students = [
+  {
+    name: "Tugay",
+    surname: "Tuna",
+    class: "JS01",
+    admin: true,
+    scores: [50, 60, 100],
+  },
+  {
+    name: "Tolga",
+    surname: "Tunas",
+    class: "JS01",
+    admin: false,
+    scores: [80, 40, 75],
+  },
+  {
+    name: "Ercan",
+    surname: "Yılmaz",
+    class: "JS01",
+    admin: true,
+    scores: [70, 60, 90],
+  },
+  {
+    name: "Ahmet",
+    surname: "Yılmaz",
+    class: "CS01",
+    admin: false,
+    scores: [70, 20, 50],
+  },
+];
+
+console.log(students);
